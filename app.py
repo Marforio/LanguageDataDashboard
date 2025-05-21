37e90ffd59ed3d54b463dadcfa9b08b778ae3ffa
Cruz Santiago <santiago.cruz@hes-so.ch>
2025-05-21 09:05:20 +0200

    port fix for deployment

--- app.py
+++ app.py
@@ -213,8 +213,12 @@ app.layout = dbc.Container([
 ], fluid=True)
 
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host='0.0.0.0', port=port)
 
-
-
+"""
 if __name__ == '__main__':
     app.run(debug=True)
+"""
+
