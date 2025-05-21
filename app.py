adb3f166bf3764277894ad05b33336b602d244b5
Cruz Santiago <santiago.cruz@hes-so.ch>
2025-05-21 09:43:29 +0200

    dash app run command fix

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def health_check():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 
